e4070cc69c2abfc9cb184e7d3a53fe4d6d3e8695
RAJ-Git18 <simkhadaraj98@gmail.com>
2026-02-26 21:35:31 +0545

    fix: refined prompt and assigned role invocation

--- services/data_extraction_service.py
+++ services/data_extraction_service.py
@@ -1,5 +1,5 @@
 import io
-from PyPDF2 import PdfReader
+import pymupdf
 from utils.logger import logger
 from google import genai
 from pydantic import BaseModel, Field
@@ -7,6 +7,7 @@ from typing import List, Optional
 from schemas.request.predict_lifespan import PredictLifespanRequest
 from dotenv import load_dotenv
 import json
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 
 load_dotenv()
@@ -18,38 +19,56 @@ class DataExtractionService:
         self.file = file
 
     async def extract_data(self):
-        byte_content = await self.file.read()
-
-        # Create a BytesIO object from the byte content
-        pdf_file_obj = io.BytesIO(byte_content)
-
-        reader = PdfReader(pdf_file_obj)
-        extracted_text = reader.pages[0].extract_text()
-        logger.info(f"Extracted Text: {extracted_text}")
+        pdf_bytes = await self.file.read()
+        pdf_stream = io.BytesIO(pdf_bytes)
+        pdf = pymupdf.open(stream=pdf_stream, filetype="pdf")
+        extracted_text = ""
+        for page in pdf:
+            extracted_text += page.get_text()
         return extracted_text
 
-    def get_prompt(self, extracted_text: str):
-        return f"""
-        You are a data extraction expert. Your task is to extract information from the following text and return it in JSON format.
-        
-        Extracted Text:
-        {extracted_text}
-        
-        Return the extracted information in the JSON format.
+    def get_system_prompt(self) -> str:
+        return """You are a data extraction expert. Your task is to extract information from the following text and return it in JSON format as per the schema.
+Example:
+{{
+  "adult_mortality": 10,
+  "infant_deaths": 0,
+  "under_five_deaths": 0,
+  "alcohol": 194,
+  "percentage_expenditure": 0,
+  "hepatitis_b": 553,
+  "measles": 0,
+  "bmi": 34,
+  "polio": 19,
+  "total_expenditure": 226,
+  "diphtheria": 19,
+  "hiv_aids": 0,
+  "gdp": 448,
+  "population": 652,
+  "thinness_1_19_years": 34,
+  "thinness_5_9_years": 34,
+  "income_composition_of_resources": 167,
+  "schooling": 12.4
+}}
         """
+
+    def get_user_prompt(self, extracted_text: str) -> str:
+        return f"Extracted Text:\n{extracted_text}"
 
     async def create_request_model(self) -> PredictLifespanRequest:
         extracted_text = await self.extract_data()
-        prompt = self.get_prompt(extracted_text)
-        response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            contents=prompt,
-            config={
-                "response_mime_type": "application/json",
-                "response_json_schema": PredictLifespanRequest.model_json_schema(),
-            },
+        system_content = self.get_system_prompt()
+        user_content = self.get_user_prompt(extracted_text)
+        prompt = [
+            ("system", system_content),
+            ("human", user_content),
+        ]
+        logger.info(f"Prompt ----------> \n{prompt}")
+        model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+        structured_model = model.with_structured_output(
+            schema=PredictLifespanRequest.model_json_schema(), method="json_schema"
         )
-        py_dict = json.loads(response.text)
-        logger.info(f"Type of response: {type(py_dict)}")
-        logger.info(f"Response: {py_dict}")
-        return PredictLifespanRequest(**py_dict)
+        response = structured_model.invoke(prompt)
+        logger.info(f"Type of response: {type(response)}")
+        logger.info(f"Response: {json.dumps(response, indent=2)}")
+        return PredictLifespanRequest(**response)
